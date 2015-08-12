4c6d96eb6d97ee559619c852ae88d0729a667a22
abhisekz <mr.abhisekroy@gmail.com>
2015-08-13 01:43:47 +0530

    Added the style task in Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,24 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-ruby-sass'),
+	autoprefix = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	rename = require('gulp-rename');
 
+// Setup the server at port 4000
 gulp.task('express', function() {
   var express = require('express');
   var app = express();
   app.use(express.static(__dirname));
   app.listen(4000, '0.0.0.0');
+});
+
+// Setting up the style section
+gulp.task('style', function() {
+    return sass('sass', { style : 'expanded' })
+    	.pipe(gulp.dest('css'))
+    	.pipe(rename({ suffix : '.min' }))
+    	.pipe(minifycss())
+    	.pipe(gulp.dest('css'))
 });
 
 gulp.task('default', ['express'], function() {
