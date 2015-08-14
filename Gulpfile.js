4aa597e539cedd1218502f13cc07a9f6db9c4d71
abhisekz <mr.abhisekroy@gmail.com>
2015-08-14 12:10:35 +0530

    Added the sass folder structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,26 +1,47 @@
 var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
-	autoprefix = require('gulp-autoprefixer'),
-	minifycss = require('gulp-minify-css'),
-	rename = require('gulp-rename');
+        sass = require('gulp-ruby-sass'),
+        autoprefixer = require('gulp-autoprefixer'),
+        minifycss = require('gulp-minify-css'),
+        rename = require('gulp-rename');
 
-// Setup the server at port 4000
 gulp.task('express', function() {
   var express = require('express');
   var app = express();
+  app.use(require('connect-livereload')({port: 35729}));
   app.use(express.static(__dirname));
   app.listen(4000, '0.0.0.0');
 });
 
-// Setting up the style section
-gulp.task('style', function() {
-    return sass('sass', { style : 'expanded' })
-    	.pipe(gulp.dest('css'))
-    	.pipe(rename({ suffix : '.min' }))
-    	.pipe(minifycss())
-    	.pipe(gulp.dest('css'))
+var tinylr;
+gulp.task('livereload', function() {
+  tinylr = require('tiny-lr')();
+    tinylr.listen(35729);
 });
 
-gulp.task('default', ['express'], function() {
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(__dirname, event.path);
+
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
+gulp.task('styles', function() {
+  return sass('sass', { style: 'expanded' })
+    .pipe(gulp.dest('css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('sass/*.scss', ['styles']);
+  gulp.watch('*.html', notifyLiveReload);
+  gulp.watch('css/*.css', notifyLiveReload);
+});
+
+gulp.task('default', ['styles', 'express', 'livereload', 'watch'], function() {
 
 });
