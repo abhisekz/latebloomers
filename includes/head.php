d9f5b19ed8571d1f67e17a3a39b46b5b4db3856b
abhisekz <mr.abhisekroy@gmail.com>
2015-09-10 23:39:05 +0530

    Fixed the responsive issue

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,8 @@
 <?php $baseUrl = 'http://www.latebloomersrehab.org' ?>
 
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $baseUrl; ?>/icons/apple-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $baseUrl; ?>/icons/apple-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $baseUrl; ?>/icons/apple-icon-72x72.png">
@@ -17,9 +20,16 @@
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="<?php echo $baseUrl; ?>/icons/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
+
 <link rel="stylesheet" href="<?php echo $baseUrl; ?>/css/style.css">
+
 <link rel="stylesheet" href="<?php echo $baseUrl; ?>/css/lib/sweetalert.css">
-    <script src="<?php echo $baseUrl; ?>/js/sweetalert-dev.js"></script>
+<script src="<?php echo $baseUrl; ?>/js/sweetalert-dev.js"></script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
 <!--[if lt IE 9]>
-    <script src="bower_components/html5shiv/dist/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
