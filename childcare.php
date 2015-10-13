721eadad5769e205afc0f78876903b48720cacbc
abhisekz <mr.abhisekroy@gmail.com>
2015-10-14 00:33:40 +0530

    Added the childcare image and page

--- childcare.php
+++ childcare.php
@@ -15,12 +15,29 @@
         <div class="container">
             <article>
                 <h2 class="section-hdr page-title">Child Care</h2>
-                <div class="content-area">
-                    <section>
-                        <div class="under-construction">
-                            This page is under construction <br> and will be <br>coming to you soon.
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="box-child">
+                            <i style="background-image: url('<?php echo $baseUrl; ?>/images/ranjit.jpg');background-position: 44% 31%;"></i>
+                            <h2>Ranajit</h2>
+                            <p>
+                                Father's Name: Gadadhar Santra <br/>
+                                Village: Kamdahari <br/>
+                                Age: 18 yrs <br/>
+                                
+                            </p>
+                            
+                            <div class="collapse" id="collapseExample">
+                              <div class="well">
+                               Currently persuing his study in std 12 with commerce stream in the BORAL HIGH SCHOOL in Garia region.He is mentally challenged.His father is a daily wage labour.He wishes to become a teacher in future. He also dreams of giving his father a sort of financially comfortable life. His favourite pass time is playing cricket, Sachin Tendulkar is his idol.
+                              </div>
+                            </div>
+                            <a class="btn btn-primary collapsed" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                              See <span class="more">more</span><span class="less">less</span>
+                            </a>
                         </div>
-                    </section>
+                    </div>
+                </div>
                    
             </article>
             
